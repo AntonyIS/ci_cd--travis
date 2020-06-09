9813f04b1ec6bbf0a9797cacb34a07758db4290f
AntonyIS <antonyshikubu@gmail.com>
2020-06-09 23:39:13 +0300

    Edited init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-from . import views
+# from . import views
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/data', methods=['GET', 'POST'])
+def get_data():
+    title = "Add data"
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        if username == '' or email == '' or password == '' :
+            message = "Fill all fields"
+            return render_template('form.html', message=message, title=title)
+        else:
+            message = "Data received"
+            data = {
+                'username':username,
+                'email':email,
+                'password':password
+
+            }
+            return render_template(
+                'form.html',
+                message=message,
+                title=title,
+                data =data
+            )
+
+    message = "Welcome, add data"
+    return render_template('form.html', message=message, title=title)
