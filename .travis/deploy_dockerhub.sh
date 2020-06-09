64c26f297c22a64d181b7347e41259b005134c15
AntonyIS <antonyshikubu@gmail.com>
2020-06-10 00:03:31 +0300

    Edited init file

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -4,6 +4,6 @@ TAG="latest"
 else
 TAG="$TRAVIS_BRANCH"
 fi
-docker build -t $TRAVIS_REPO_SLUG:$TAG .
+docker build -t $DOCKER_USER:$TAG .
 #docker tag $TRAVIS_REPO_SLUG $DOCKER_REPO
 docker push $DOCKER_REPO
